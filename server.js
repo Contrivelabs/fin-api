a2c10b84b3a9ab8a9d6803a0f65e1df325063b96
Santhoshkumar B <jayamsantho@gmail.com>
2024-12-14 12:44:03 +0530

    updated express rate limit package

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const cors = require('cors');
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
 
 const companiesRoutes = require('./backend/routes/companies');
 const branchesRoutes = require('./backend/routes/branches');
